2e280a4d1b9465d45564384e474980ba3c6ccffc
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-09-24 14:46:29 -0700

    Added elements to remove

--- wikipedia.js
+++ wikipedia.js
@@ -5,9 +5,12 @@
 const headers = document.querySelectorAll('.vector-header-container')
 const toolbars = document.querySelectorAll('.vector-page-toolbar')
 const footers = document.querySelectorAll('.mw-footer-container')
+const jumps = document.querySelectorAll('.mw-jump-link')
 const scripts = document.querySelectorAll('script')
 const meta = document.querySelectorAll('meta')
 const styles = document.querySelectorAll('style')
+const inputs = document.querySelectorAll('input')
+const buttons = document.querySelectorAll('button')
 
 const elements = []
 elements.push(document.querySelector('#p-lang-btn'))
@@ -17,13 +20,18 @@ elements.push(document.querySelector('#vector-page-tools-pinned-container'))
 const removeElements = (array) => {
     array.forEach(element => {
         element.remove()
-    });
+    })
 }
 
 removeElements(headers)
 removeElements(toolbars)
-removeElements(scripts)
 removeElements(footers)
+removeElements(jumps)
+removeElements(scripts)
+removeElements(meta)
+removeElements(styles)
+removeElements(inputs)
+removeElements(buttons)
 
 removeElements(elements)
 
